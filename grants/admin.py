88d7ac42f3f25a935f3f82f6cccf474f512120b4
kevin <2024ambchb085f@kab.ac.ug>
2026-08-09 06:54:21 +0300

    Delete grants directory

--- grants/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ReadingTrack, StudentProfile, ReadingLog, WithdrawalRequest
-
-admin.site.register(ReadingTrack)
-admin.site.register(StudentProfile)
-admin.site.register(ReadingLog)
-admin.site.register(WithdrawalRequest)
