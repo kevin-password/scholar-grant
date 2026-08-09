b1e2f4de1f90711ba3f68faecbd9ecbbd19a5099
kevin <2024ambchb085f@kab.ac.ug>
2026-08-09 06:53:50 +0300

    Delete library directory

--- library/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    # FIX: Changed 'total_pages' to 'pages' and added 'genre'
-    list_display = ('title', 'author', 'pages', 'genre')
-    search_fields = ('title', 'author')
